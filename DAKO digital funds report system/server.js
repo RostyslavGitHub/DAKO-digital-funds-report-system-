d12f8af6849fbe38569bbbe084f2da09ea5386f6
RostyslavGitHub <117546122+RostyslavGitHub@users.noreply.github.com>
2023-07-10 16:03:18 +0300

    Add files via upload

--- DAKO digital funds report system/server.js
+++ DAKO digital funds report system/server.js
@@ -1,80 +1,138 @@
 const fs = require('fs');
 const path = require('path');
+const crypto = require('crypto');
 
-function processDirectories(directoryPath) {
-    const directories = fs.readdirSync(directoryPath);
+function calculateMD5(filePath) {
+  return new Promise((resolve, reject) => {
+    const hash = crypto.createHash('md5');
+    const stream = fs.createReadStream(filePath);
 
-    directories.forEach(dir => {
-        const dirPath = path.join(directoryPath, dir);
-        const folderInfo = processDirectory(dirPath);
-        const folderName = path.basename(dirPath);
-
-        const fundNumber = getFundNumber(folderName);
-        const descriptionNumber = getDescriptionNumber(folderName);
-        const caseNumber = getCaseNumber(folderName);
-
-        console.log('{"№№\\nз/п": "",');
-        console.log('"№ фонду": "' + fundNumber + '",');
-        console.log('"№ опису": "' + descriptionNumber + '",');
-        console.log('"№ справи": "' + caseNumber + '",');
-        console.log('"Розмір в байтах": "' + folderInfo.totalSize + '",');
-        console.log('"Кількість файлів": "' + folderInfo.fileCount + '",');
-        console.log('"Кількість слайдів": "' + folderInfo.tifCount + '",');
-        console.log('"Геш-значення файла (найменування алгоритму криптографічного гешування)": "",');
-        console.log('"Примітка": ""');
-        console.log('},');
+    stream.on('data', (data) => {
+      hash.update(data);
     });
+
+    stream.on('end', () => {
+      const md5Hash = hash.digest('hex');
+      resolve(md5Hash);
+    });
+
+    stream.on('error', (error) => {
+      reject(error);
+    });
+  });
 }
 
 function processDirectory(directoryPath) {
-    let totalSize = 0;
-    let fileCount = 0;
-    let tifCount = 0;
+  let totalSize = 0;
+  let fileCount = 0;
+  let tifCount = 0;
 
-    const files = fs.readdirSync(directoryPath);
+  const files = fs.readdirSync(directoryPath);
 
-    files.forEach(file => {
-        const filePath = path.join(directoryPath, file);
-        const stats = fs.statSync(filePath);
+  files.forEach((file) => {
+    const filePath = path.join(directoryPath, file);
+    const stats = fs.statSync(filePath);
 
-        if (stats.isFile()) {
-            totalSize += stats.size;
-            fileCount++;
-            if (file.endsWith('.tif')) {
-                tifCount++;
-            }
-        } else if (stats.isDirectory()) {
-            const subFolderInfo = processDirectory(filePath);
-            totalSize += subFolderInfo.totalSize;
-            fileCount += subFolderInfo.fileCount;
-            tifCount += subFolderInfo.tifCount;
-        }
-    });
+    if (stats.isFile()) {
+      totalSize += stats.size;
+      fileCount++;
+      if (file.endsWith('.tif')) {
+        tifCount++;
+      }
+    }
+  });
 
-    return { totalSize, fileCount, tifCount };
+  return { totalSize, fileCount, tifCount };
 }
 
 function getFundNumber(folderName) {
-    const startIndex = folderName.indexOf('DAKO-') + 5;
-    const endIndex = folderName.indexOf('-', startIndex);
-    return folderName.substring(startIndex, endIndex).trim();
+  const startIndex = folderName.indexOf('DAKO-') + 5;
+  const endIndex = folderName.indexOf('-', startIndex);
+  return folderName.substring(startIndex, endIndex).trim();
 }
 
 function getDescriptionNumber(folderName) {
-    const startIndex = folderName.indexOf('-', folderName.indexOf('DAKO-')) + 1;
-    const endIndex = folderName.lastIndexOf('-');
-    const descriptionPart = folderName.substring(startIndex, endIndex).trim();
-    const descriptionParts = descriptionPart.split('-');
-    if (descriptionParts.length > 1) {
-        return descriptionParts[1].trim();
-    }
-    return '';
+  const startIndex = folderName.indexOf('-', folderName.indexOf('DAKO-')) + 1;
+  const endIndex = folderName.lastIndexOf('-');
+  const descriptionPart = folderName.substring(startIndex, endIndex).trim();
+  const descriptionParts = descriptionPart.split('-');
+  if (descriptionParts.length > 1) {
+    return descriptionParts[1].trim();
+  }
+  return '';
 }
 
 function getCaseNumber(folderName) {
-    const parts = folderName.split('-');
-    return parts[parts.length - 1].trim();
+  const parts = folderName.split('-');
+  return parts[parts.length - 1].trim();
 }
 
-const directoryPath = path.join(__dirname, '/all'); // шлях до теки з фондами
-processDirectories(directoryPath);
+function processArchives(zipDirectoryPath, allDirectoryName) {
+  const archives = fs.readdirSync(zipDirectoryPath);
+
+  archives.forEach((archive) => {
+    const archivePath = path.join(zipDirectoryPath, archive);
+    const stats = fs.statSync(archivePath);
+
+    if (stats.isFile()) {
+      const folderName = archive.split('.')[0];
+
+      if (folderName === allDirectoryName) {
+        calculateMD5(archivePath)
+          .then((md5Hash) => {
+            console.log('Архів:', archivePath);
+            console.log('MD5 геш-значення:', md5Hash);
+            console.log('---');
+          })
+          .catch((error) => {
+            console.error('Помилка обчислення MD5 геш-значення для архіву', archivePath);
+            console.error('Помилка:', error);
+            console.log('---');
+          });
+      }
+    }
+  });
+}
+
+async function processDirectories(directoryPath, zipDirectoryPath) {
+  const directories = fs.readdirSync(directoryPath);
+
+  for (const dir of directories) {
+    const dirPath = path.join(directoryPath, dir);
+    const folderInfo = processDirectory(dirPath);
+    const folderName = path.basename(dirPath);
+    const zipFilePath = path.join(zipDirectoryPath, folderName + '.zip');
+    let md5Hash = ''; 
+
+    if (fs.existsSync(zipFilePath)) {
+      try {
+        md5Hash = await calculateMD5(zipFilePath); 
+      } catch (error) {
+        console.error('Помилка обчислення MD5 геш-значення для архіву', zipFilePath);
+        console.error('Помилка:', error);
+        console.log('---');
+      }
+    }
+
+    console.log('{"№№\\nз/п": "",');
+    console.log('"№ фонду": "' + getFundNumber(folderName) + '",');
+    console.log('"№ опису": "' + getDescriptionNumber(folderName) + '",');
+    console.log('"№ справи": "' + getCaseNumber(folderName) + '",');
+    console.log('"Розмір в байтах": "' + folderInfo.totalSize + '",');
+    console.log('"Кількість файлів": "' + folderInfo.fileCount + '",');
+    console.log('"Кількість слайдів": "' + folderInfo.tifCount + '",');
+    console.log('"Геш-значення файла (MD5)": "' + md5Hash + '",');
+    console.log('"Примітка": ""');
+    console.log('},');
+  }
+}
+
+const zipDirectoryPath = path.join(__dirname, 'allInZip'); // шлях до теки з архівованими у zip фондами
+const directoryPath = path.join(__dirname, 'all'); // шлях до теки з фондами
+
+(async () => {
+  await processArchives(zipDirectoryPath);
+  await processDirectories(directoryPath, zipDirectoryPath);
+})();
+
+
